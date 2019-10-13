87f6e86fd11f5bde12f2d0eb59e4841056c972e8
Mona Moinipour <monam@Monas-MacBook-Pro.local>
2019-10-13 18:42:26 +0200

    Enrollment function

--- src/app/components/detail-kurs/detail-kurs.component.ts
+++ src/app/components/detail-kurs/detail-kurs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
+import { createAotUrlResolver } from '@angular/compiler';
 
 @Component({
   selector: 'app-detail-kurs',
@@ -12,17 +13,38 @@ export class DetailKursComponent implements OnInit {
 
   public courseID;
   public courseData;
+  public isCollapsed : boolean = false;
+  public myCourse;
+  public getObject1;
+  public storage;
+  
+  
 
   constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
-
+  
     this.courseID = this.route.snapshot.paramMap.get('id');
 
     this.http.get('https://hochschulsport-koeln.de/json/course/' + this.courseID + '/?_format=json')
       .subscribe((data) => {
         this.courseData = data;
       });
-  }
+  }  
 
+  public enroll() {
+    this.isCollapsed = !this.isCollapsed;
+    for(const course of this.courseData){
+      this.myCourse = [{"title":""+course.title+"","id":"" +course.nid+""}];
+    }
+
+    this.getObject1 = JSON.parse(localStorage.getItem('storeObj'));
+    this.storage= this.myCourse
+    if(this.getObject1 != null){
+      this.storage = this.myCourse.concat(this.getObject1);
+        
+    }
+
+    localStorage.setItem('storeObj', JSON.stringify(this.storage));
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,13 +10,19 @@ export class HomeComponent implements OnInit {
 
   public news;
   public topcourses;
+  public getEnrolled;
+
 
   constructor(private http: HttpClient) {
     this.news = [];
     this.topcourses = [];
+    this.getEnrolled = [];
+
   }
 
   ngOnInit() {
+
+    this.getEnrolled = JSON.parse(localStorage.getItem('storeObj'));
 
     this.http.get('https://hochschulsport-koeln.de/json/news?_format=json')
       .subscribe((data) => {
